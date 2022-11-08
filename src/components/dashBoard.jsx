bb02a0df1ac3543e9c9ffd2fae6148c2690e02f7
Indira Kumar <indirakumarak@gmail.com>
2022-11-08 22:07:02 +0530

    chart created

--- src/components/dashBoard.jsx
+++ src/components/dashBoard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import d3 from "d3";
-const dashBoard = () => {
-  let csvFileName =
-    "../Currency_Conversion_Test_Data/Exchange_Rate_Report_2012.csv";
-  d3.csv(csvFileName).then((loadedCurrenyCsvData) => {
-    console.log(loadedCurrenyCsvData);
-  });
-  return <div>dashBoard</div>;
+import Barchart from "./Barchart";
+import OptionButton from "./OptionButton";
+const DashBoard = () => {
+  return (
+    <div>
+      <OptionButton />
+      <OptionButton />
+      <Barchart />
+      Here is dashboard
+    </div>
+  );
 };
 
-export default dashBoard;
+export default DashBoard;
