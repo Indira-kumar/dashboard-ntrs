651a499b08ebb3b79f17271c56bc87604a26aca7
Indira Kumar <indirakumarak@gmail.com>
2022-11-09 08:24:55 +0530

    App.js Final Layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import DashBoard from "./components/DashBoard";
 import ProjectTitle from "./components/ProjectTitle";
+import TextExplanation from "./components/TextExplanation";
 import "semantic-ui-css/semantic.min.css";
 function App() {
   return (
     <div>
       <ProjectTitle />
+      <TextExplanation />
       <DashBoard />
     </div>
   );
